9e6a9a0569fba87d076a38e09e10a5a3f0c9e0ea
Stephen Lacy <zenko44@gmail.com>
2021-07-08 18:37:15 +0100

    Update SudokuSolver.py
    The attempt to make it 3D. working on making the solver work over a 3D array is proving more difficult than originally thought. Fuckin thing. creation of the new branch to overwrite the effectively  functional original 2D solver

--- SudokuSolver.py
+++ SudokuSolver.py
@@ -10,17 +10,78 @@ from array import *
 #3. in the 3x3 are the can be 0 duplicate numbers,
 # this rule also applies to rows and columns on the 
 # board as a whole
-sudoku_array = ([0, 0, 0,  0, 0, 0,  0, 0, 0],
+sudoku_array = ([[0, 0, 0,  0, 0, 0,  0, 0, 0],
                 [0, 0, 0,  0, 0, 0,  0, 0, 0],
-                [0, 0, 0,  0, 0, 9,  0, 0, 0],
+                [0, 0, 0,  0, 0, 0,  0, 0, 0],
                 
                 [0, 0, 0,  0, 0, 0,  0, 0, 0],
-                [0, 0, 0,  0, 0, 0,  0, 0, 0], 
-                [0, 0, 0,  0, 0, 0,  0, 0, 0],
+                [0, 0, 0,  0, 0, 0,  0, 0, 0],          ## Front face of the sudoku cube
+                [0, 0, 0,  0, 0, 0,  0, 0, 0],          ## [0][0 - 8][0 - 8]
 
                 [0, 0, 0,  0, 0, 0,  0, 0, 0],
                 [0, 0, 0,  0, 0, 0,  0, 0, 0],
-                [0, 0, 0,  0, 0, 0,  0, 0, 0])
+                [0, 0, 0,  0, 0, 0,  0, 0, 0]],
+
+                [[0, 0, 0,  0, 0, 0,  0, 0, 0],
+                [0, 0, 0,  0, 0, 0,  0, 0, 0],
+                [0, 0, 0,  0, 0, 0,  0, 0, 0],
+
+                [0, 0, 0,  0, 0, 0,  0, 0, 0],
+                [0, 0, 0,  0, 0, 0,  0, 0, 0],          # Top face of sudoku cube
+                [0, 0, 0,  0, 0, 0,  0, 0, 0],          # [1][0 - 8][0 - 8]
+
+                [0, 0, 0,  0, 0, 0,  0, 0, 0],
+                [0, 0, 0,  0, 0, 0,  0, 0, 0],
+                [0, 0, 0,  0, 0, 0,  0, 0, 0]],
+
+                [[0, 0, 0,  0, 0, 0,  0, 0, 0],
+                [0, 0, 0,  0, 0, 0,  0, 0, 0],
+                [0, 0, 0,  0, 0, 0,  0, 0, 0],
+
+                [0, 0, 0,  0, 0, 0,  0, 0, 0],
+                [0, 0, 0,  0, 0, 0,  0, 0, 0],  # Bottom Face of sudoku cube
+                [0, 0, 0,  0, 0, 0,  0, 0, 0],  # [2][0 - 8][0 - 8]
+
+                [0, 0, 0,  0, 0, 0,  0, 0, 0],
+                [0, 0, 0,  0, 0, 0,  0, 0, 0],
+                [0, 0, 0,  0, 0, 0,  0, 0, 0]],
+
+                [[0, 0, 0,  0, 0, 0,  0, 0, 0],
+                [0, 0, 0,  0, 0, 0,  0, 0, 0],
+                [0, 0, 0,  0, 0, 0,  0, 0, 0],
+
+                [0, 0, 0,  0, 0, 0,  0, 0, 0],
+                [0, 0, 0,  0, 0, 0,  0, 0, 0],  # Left face of Sudoku Cube
+                [0, 0, 0,  0, 0, 0,  0, 0, 0],  # [3][0 - 8][0 - 8]
+
+                [0, 0, 0,  0, 0, 0,  0, 0, 0],
+                [0, 0, 0,  0, 0, 0,  0, 0, 0],
+                [0, 0, 0,  0, 0, 0,  0, 0, 0]],
+
+                [[0, 0, 0,  0, 0, 0,  0, 0, 0],
+                [0, 0, 0,  0, 0, 0,  0, 0, 0],
+                [0, 0, 0,  0, 0, 0,  0, 0, 0],
+
+                [0, 0, 0,  0, 0, 0,  0, 0, 0],
+                [0, 0, 0,  0, 0, 0,  0, 0, 0],  # Right face of sudoku Cube
+                [0, 0, 0,  0, 0, 0,  0, 0, 0],  # [4][0 - 8][0 - 8]
+
+                [0, 0, 0,  0, 0, 0,  0, 0, 0],
+                [0, 0, 0,  0, 0, 0,  0, 0, 0],
+                [0, 0, 0,  0, 0, 0,  0, 0, 0]],
+
+                [[0, 0, 0,  0, 0, 0,  0, 0, 0],
+                [0, 0, 0,  0, 0, 0,  0, 0, 0],
+                [0, 0, 0,  0, 0, 0,  0, 0, 0],
+
+                [0, 0, 0,  0, 0, 0,  0, 0, 0],
+                [0, 0, 0,  0, 0, 0,  0, 0, 0],  # Back face of sudoku cube
+                [0, 0, 0,  0, 0, 0,  0, 0, 0],  # [5][0 - 8][0 - 8]
+
+                [0, 0, 0,  0, 0, 0,  0, 0, 0],
+                [0, 0, 0,  0, 0, 0,  0, 0, 0],
+                [0, 0, 0,  0, 0, 0,  0, 0, 0]])
+
 
 
 def print_board(board):
@@ -39,29 +100,29 @@ def print_board(board):
 
 
 def find_empty(board):
-    for i in range(len(board)):
-        for x in range(len(board[0])):
-            if board[i][x] == 0:
-                return (i, x)  # row, col
+    for z in range(len(board)):
+        for i in range(len(board[0])):
+            for x in range(len(board[1])):
+                if board[z][i][x] == 0:
+                    return (z, i, x)  # row, col
 
     return None
-
 
 def solve(board):
     find = find_empty(board)
     if not find:
         return True
     else:
-        row, col = find
+        face, row, col = find
 
     for i in range(1, 10):
-        if valid(board, i, (row, col)):
-            board[row][col] = i
+        if valid(board, i, (face, row, col)):
+            board[face][row][col] = i
 
             if solve(board):
                 return True
 
-            board[row][col] = 0
+            board[face][row][col] = 0
 
     return False
 
@@ -77,38 +138,46 @@ def valid(board, num, pos):
         if board[i][pos[1]] == num and pos[0] != i:
             return False
 
+
     # Check box
     box_x = pos[1] // 3
     box_y = pos[0] // 3
+    box_z = pos[2] // 3
 
-    for i in range(box_y*3, box_y*3 + 3):
-        for x in range(box_x * 3, box_x*3 + 3):
-            if board[i][x] == num and (i, x) != pos:
-                return False
+    for z in range(box_z * 3, box_z * 3 + 3):
+        for i in range(box_y * 3, box_y * 3 + 3):
+            for x in range(box_x * 3, box_x * 3 + 3):
+                if board[i][x] == num and (i, x) != pos:
+                    return False
 
     return True
 
 
 def print_board(board):
-    for i in range(len(board)):
-        if i % 3 == 0 and i != 0:
-            print("+ + + + + + + + + + + + + ")
+    for z in range(len(board)):
+        if z % 3 == 0 and z != 0:
+            print("......................")
+        for i in range(len(board[0])):
+            if i % 3 == 0 and i != 0:
+                print("---------------------------")
 
-        for x in range(len(board[0])):
-            if x % 3 == 0 and x != 0:
-                print(" | ", end="")
+            for x in range(len(board[1])):
+                if x % 3 == 0 and x != 0:
+                    print(" | ", end="")
 
-            if x == 8:
-                print(board[i][x])
-            else:
-                print(str(board[i][x]) + " ", end="")
+                if x == 8:
+                    print(board[z][i][x])
+                else:
+                    print(str(board[z][i][x]) + " ", end="")
+                   
 
 
 def find_empty(board):
-    for i in range(len(board)):
-        for x in range(len(board[0])):
-            if board[i][x] == 0:
-                return (i, x)  # row, col
+    for z in range(len(board)):
+        for i in range(len(board[0])):
+            for x in range(len(board[1])):
+                if board[z][i][x] == 0:
+                    return (z, i, x)  # row, col
 
     return None
 
